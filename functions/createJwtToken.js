08ad8ec740c663b1f4f99bb7ded5f3f2421172d8
pookiepew <glensorbo@gmail.com>
2021-02-15 16:50:30 +0100

    Updates/cleans createJwtToken function

--- functions/createJwtToken.js
+++ functions/createJwtToken.js
@@ -1,10 +1,7 @@
-// const jwt = require("jsonwebtoken");
-
 module.exports = createJwtToken = async (user, jwt, config) => {
   const payload = { user: { id: user._id } };
-  console.log(config.JWT_SECRET);
   try {
-    let token = jwt.sign(payload, config.JWT_SECRET, { expiresIn: "5 days" });
+    const token = jwt.sign(payload, config.JWT_SECRET, { expiresIn: '5 days' });
     return token;
   } catch (error) {
     throw error;
